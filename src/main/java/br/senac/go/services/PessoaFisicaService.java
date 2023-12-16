be083e87dbb906947555bce20724092115057486
Daniel Gondim <danielgondimdecastro@gmail.com>
2023-12-16 17:04:19 -0300

    feat: adiciona classe PessoaFisicaService
    - Implementa a classe PessoaFisicaService, que fornece serviços para manipulação de dados relacionados a PessoaFisica.
    - A classe implementa a interface IService, que define métodos padrão para operações CRUD.
    - Métodos CRUD estão atualmente retornando nulo, fornecendo uma estrutura básica para implementações futuras.
    - Essa classe servirá como a camada de serviço para operações relacionadas a PessoaFisica, encapsulando a lógica de negócios associada.

---
+++ src/main/java/br/senac/go/services/PessoaFisicaService.java
@@ -0,0 +1,41 @@
+package br.senac.go.services;
+
+import br.senac.go.domain.PessoaFisica;
+import br.senac.go.generics.IService;
+
+public class PessoaFisicaService implements IService<PessoaFisica, Integer> {
+    @Override
+    public PessoaFisica create(PessoaFisica entity) {
+        return null;
+    }
+
+    @Override
+    public PessoaFisica readById(Integer id) throws Exception {
+        return null;
+    }
+
+    @Override
+    public PessoaFisica read(PessoaFisica entity) throws Exception {
+        return null;
+    }
+
+    @Override
+    public PessoaFisica updatePatch(PessoaFisica entity, Integer id) throws Exception {
+        return null;
+    }
+
+    @Override
+    public PessoaFisica updatePut(PessoaFisica entity, Integer id) {
+        return null;
+    }
+
+    @Override
+    public PessoaFisica deleteById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public PessoaFisica delete(PessoaFisica entity) {
+        return null;
+    }
+}
